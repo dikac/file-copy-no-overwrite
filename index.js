db225b6b79a48bb803addd63a2147af4be41e55d
dikac <rigel234@yahoo.com>
2019-10-29 05:03:16 +0700

    switch root to process.env.INIT_CWD

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 #!/usr/bin/env node
 'use strict';
-const root = require('app-root-path');
+
 const Fs = require('fs');
 
-console.log(process);
-console.log(root);
+const root = process.env.INIT_CWD;
 
-console.log(process.argv);
+let [, , target, dest] = process.argv;
 
-// let [, , target, dest] = process.argv;
-//
-// if(!target) {
-//     throw new Error('target is not provided');
-// } else {
-//     target = root + '/' + target;
-// }
-//
-// if(!dest) {
-//     throw new Error('destination is not provided');
-// } else {
-//     dest = root + '/' + dest;
-// }
-//
-// if(!Fs.existsSync(dest)) {
-//
-//     Fs.copyFileSync(target,  dest);
-//     console.log(`copying ${target} to ${dest}`);
-//
-// } else {
-//
-//     console.log('already exist');
-// }
+if(!target) {
+    throw new Error('target is not provided');
+} else {
+    target = root + '/' + target;
+}
+
+if(!dest) {
+    throw new Error('destination is not provided');
+} else {
+    dest = root + '/' + dest;
+}
+
+if(!Fs.existsSync(dest)) {
+
+    Fs.copyFileSync(target,  dest);
+    console.log(`copying ${target} to ${dest}`);
+
+} else {
+
+    console.log('already exist');
+}
 
