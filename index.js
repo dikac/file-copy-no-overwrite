92ced8cd396ad2ed07547803afab5a858d8b3b80
dikac <rigel234@yahoo.com>
2019-10-29 04:50:44 +0700

    debugging error

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ const root = require('app-root-path').path;
 const Fs = require('fs');
 
 console.log(root);
+console.log(process.argv);
 
-let [, , target, dest] = process.argv;
-
-if(!target) {
-    throw new Error('target is not provided');
-} else {
-    target = root + '/' + target;
-}
-
-if(!dest) {
-    throw new Error('destination is not provided');
-} else {
-    dest = root + '/' + dest;
-}
-
-if(!Fs.existsSync(dest)) {
-
-    Fs.copyFileSync(target,  dest);
-    console.log(`copying ${target} to ${dest}`);
-
-} else {
-
-    console.log('already exist');
-}
+// let [, , target, dest] = process.argv;
+//
+// if(!target) {
+//     throw new Error('target is not provided');
+// } else {
+//     target = root + '/' + target;
+// }
+//
+// if(!dest) {
+//     throw new Error('destination is not provided');
+// } else {
+//     dest = root + '/' + dest;
+// }
+//
+// if(!Fs.existsSync(dest)) {
+//
+//     Fs.copyFileSync(target,  dest);
+//     console.log(`copying ${target} to ${dest}`);
+//
+// } else {
+//
+//     console.log('already exist');
+// }
 
