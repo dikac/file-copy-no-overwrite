a8ce439b5f9f437b2821ef53ef3b109e355f8598
dikac <rigel234@yahoo.com>
2019-10-29 04:16:02 +0700

    initial working

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 #!/usr/bin/env node
 'use strict';
-
+const root = require('app-root-path').path;
+const Fs = require('fs');
 console.log(process.argv);
+
+let [, , target, dest] = process.argv;
+
+if(!target) {
+    throw new Error('target is not provided');
+}
+
+if(!dest) {
+    throw new Error('destination is not provided');
+}
+
+if(!Fs.existsSync(root + '/' + dest)) {
+
+    Fs.copyFileSync(root + '/' + target, root + '/' + dest);
+}
+
